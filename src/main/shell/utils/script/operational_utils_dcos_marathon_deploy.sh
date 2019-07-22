997fa962855ce5295a7fa7ff8104f5b5f648abd1
rvcoutinho <me@rvcoutinho.com>
2019-07-22 14:12:48 -0300

    - Enabling inclusion and exclusion of modules to deploy;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/shell/utils/script/operational_utils_dcos_marathon_deploy.sh
+++ src/main/shell/utils/script/operational_utils_dcos_marathon_deploy.sh
@@ -7,8 +7,10 @@ set -o errexit
 # Default parameters.
 DEBUG=false
 DEBUG_OPT=
-DCOS_CLI_CONTAINER=
+CLI_CONTAINER=
 MODULES_FILE=modules.json
+INCLUDE_MODULES=
+EXCLUDE_MODULES=
 SERVICE_CONFIG_FILE=service.json
 TEMP_SERVICE_CONFIG_FILE=temp-service.json
 PROFILE_DIRECTORY=
@@ -42,6 +44,18 @@ while :; do
 			MODULES_FILE=${2}
 			shift
 			;;
+		
+		# Modules to deploy.
+		-i|--include-modules)
+			INCLUDE_MODULES=`echo "${2}" | sed -e "s/[,;$]/\n/g"`
+			shift
+			;;
+
+		# Modules not to deploy.
+		-e|--exclude-modules)
+			EXCLUDE_MODULES=`echo "${2}" | sed -e "s/[,;$]/\n/g"`
+			shift
+			;;
 
 		# Service config file.
 		-f|--service-config-file)
@@ -63,6 +77,15 @@ while :; do
 	shift
 done
 
+# Container run prefix.
+CONTAINER_RUN=
+# If there is a CLI container to be used.
+if [ ! -z "${CLI_CONTAINER}" ]
+then
+	# Updates the container run variable.
+	CONTAINER_RUN="docker exec -i ${CLI_CONTAINER}"
+fi
+
 # Using unavaialble variables should fail the script.
 set -o nounset
 
@@ -71,16 +94,19 @@ trap - INT TERM
 
 # Print arguments if on debug mode.
 ${DEBUG} && echo "Running 'operational_utils_dcos_marathon_deploy'"
+${DEBUG} && echo "CLI_CONTAINER=${CLI_CONTAINER}"
+${DEBUG} && echo "CONTAINER_RUN=${CONTAINER_RUN}"
 ${DEBUG} && echo "BASE_DIRECTORY=${BASE_DIRECTORY}"
 ${DEBUG} && echo "MODULES_FILE=${MODULES_FILE}"
+${DEBUG} && echo "INCLUDE_MODULES=${INCLUDE_MODULES}"
+${DEBUG} && echo "EXCLUDE_MODULES=${EXCLUDE_MODULES}"
 ${DEBUG} && echo "SERVICE_CONFIG_FILE=${SERVICE_CONFIG_FILE}"
 ${DEBUG} && echo "PROFILE_DIRECTORY=${PROFILE_DIRECTORY}"
-${DEBUG} && echo "CLI_CONTAINER=${CLI_CONTAINER}"
 
 # For each child directory.
 for CURRENT_MODULE in `jq -rc ".[]" ${BASE_DIRECTORY}/${MODULES_FILE}`
 do
-	
+
 	# Gets the module information.
 	${DEBUG} && echo "CURRENT_MODULE=${CURRENT_MODULE}"
 	CURRENT_MODULE_NAME=`echo ${CURRENT_MODULE} | jq -r ".name"`
@@ -103,84 +129,88 @@ do
 		echo "${POST_DEPLOY_SCRIPT}" || \
 		echo "${CURRENT_MODULE_POST_DEPLOY_SCRIPT}"`
 	${DEBUG} && echo "CURRENT_MODULE_POST_DEPLOY_SCRIPT=${CURRENT_MODULE_POST_DEPLOY_SCRIPT}"
-	
-	# Goes to the module directory.
-	cd ${CURRENT_MODULE_DIRECTORY}
-	
-	# Exports variables to scripts.
-	for ENV_VARIABLE in `cat ${CURRENT_MODULE_SERVICE_CONFIG} | jq -c -r '.env | keys[]'`
-	do
-		ENV_VARIABLE_VALUE="`cat ${CURRENT_MODULE_SERVICE_CONFIG} | jq -r ".env.${ENV_VARIABLE}"`"
-		${DEBUG} && echo "Exporting ariable ${ENV_VARIABLE}=${ENV_VARIABLE_VALUE} for scripts."
-		export ${ENV_VARIABLE}
-	done
-	${DEBUG} && echo "Exporting ariable CLI_CONTAINER=${CLI_CONTAINER} for scripts."
-	export CLI_CONTAINER
 
-	# If there is a pre deploy script.
-	if [ -f ${CURRENT_MODULE_PRE_DEPLOY_SCRIPT} ]
+	# If the module should be deployed.
+	if ([ -z "${INCLUDE_MODULES}" ] || \
+			echo "${INCLUDE_MODULES}" | grep "^${CURRENT_MODULE_NAME}$") && 
+		([ -z "${EXCLUDE_MODULES}" ] || \
+			! echo "${EXCLUDE_MODULES}" | grep "^${CURRENT_MODULE_NAME}$")
 	then
 	
-		# Runs the pre deploy script.
-		${DEBUG} && echo "Running ${CURRENT_MODULE_PRE_DEPLOY_SCRIPT}"
-		chmod +x ${CURRENT_MODULE_PRE_DEPLOY_SCRIPT} || true
-		${CURRENT_MODULE_PRE_DEPLOY_SCRIPT}
-	
-	fi
+		# Goes to the module directory.
+		cd ${CURRENT_MODULE_DIRECTORY}
 		
-	# If there is a service config.
-	if [ -f ${CURRENT_MODULE_SERVICE_CONFIG} ]
-	then
-	
-		# If no profile is set.
-		if [ "${PROFILE_DIRECTORY}" = "" ]
+		# If there is a service config.
+		if [ -f ${CURRENT_MODULE_SERVICE_CONFIG} ]
 		then
-			# The temporary service config is the original one.
-			cp ${CURRENT_MODULE_SERVICE_CONFIG} ${TEMP_SERVICE_CONFIG_FILE}
-		# If a profile is set.
-		else 
-			# Merges the main file with the profile file into the temporary service file.
-			jq -s '.[0] * .[1]' ${CURRENT_MODULE_SERVICE_CONFIG} \
-				${PROFILE_DIRECTORY}/${CURRENT_MODULE_SERVICE_CONFIG} > ${TEMP_SERVICE_CONFIG_FILE}
+		
+			# If no profile is set.
+			if [ "${PROFILE_DIRECTORY}" = "" ]
+			then
+				# The temporary service config is the original one.
+				cp ${CURRENT_MODULE_SERVICE_CONFIG} ${TEMP_SERVICE_CONFIG_FILE}
+			# If a profile is set.
+			else 
+				# Merges the main file with the profile file into the temporary service file.
+				jq -s '.[0] * .[1]' ${CURRENT_MODULE_SERVICE_CONFIG} \
+					${PROFILE_DIRECTORY}/${CURRENT_MODULE_SERVICE_CONFIG} > ${TEMP_SERVICE_CONFIG_FILE}
+			fi
+
+			# Exports variables to scripts.
+			for ENV_VARIABLE in `cat ${TEMP_SERVICE_CONFIG_FILE} | jq -c -r '.env | keys[]'`
+			do
+				ENV_VARIABLE_VALUE="`cat ${TEMP_SERVICE_CONFIG_FILE} | jq -r ".env.${ENV_VARIABLE}"`"
+				${DEBUG} && echo "Exporting ariable ${ENV_VARIABLE}=${ENV_VARIABLE_VALUE} for scripts."
+				export ${ENV_VARIABLE}
+			done
+			${DEBUG} && echo "Exporting ariable CLI_CONTAINER=${CLI_CONTAINER} for scripts."
+			export CLI_CONTAINER
+			
 		fi
 	
-		# If the dcos_deploy_marathon is available.
-		if command -v dcos_deploy_marathon
+		# If there is a pre deploy script.
+		if [ -f ${CURRENT_MODULE_PRE_DEPLOY_SCRIPT} ]
 		then
 		
-			# Deploys the module.
-			${DEBUG} && echo "dcos_deploy_marathon ${DEBUG_OPT} < ${TEMP_SERVICE_CONFIG_FILE}"
-			dcos_deploy_marathon ${DEBUG_OPT} < ${TEMP_SERVICE_CONFIG_FILE}
-		
-		# If the dcos_deploy_marathon is not available.
-		else 
-		
-			# Deploys the module.
-			${DEBUG} && echo "docker exec -i ${CLI_CONTAINER} \
-				dcos_deploy_marathon ${DEBUG_OPT} < ${TEMP_SERVICE_CONFIG_FILE}"
-			docker exec -i ${CLI_CONTAINER} \
-				dcos_deploy_marathon ${DEBUG_OPT} < ${TEMP_SERVICE_CONFIG_FILE}
+			# Runs the pre deploy script.
+			${DEBUG} && echo "Running ${CURRENT_MODULE_PRE_DEPLOY_SCRIPT}"
+			chmod +x ${CURRENT_MODULE_PRE_DEPLOY_SCRIPT} || true
+			${CURRENT_MODULE_PRE_DEPLOY_SCRIPT}
 		
 		fi
 			
-		# Removes the temporary.
-		rm -f ${TEMP_SERVICE_CONFIG_FILE}
+		# If there is a service config.
+		if [ -f ${TEMP_SERVICE_CONFIG_FILE} ]
+		then
 		
+			# Deploys the module.
+			${DEBUG} && echo "${CONTAINER_RUN} dcos_deploy_marathon ${DEBUG_OPT} < ${TEMP_SERVICE_CONFIG_FILE}"
+#			${CONTAINER_RUN} dcos_deploy_marathon ${DEBUG_OPT} < ${TEMP_SERVICE_CONFIG_FILE}
+				
+			# Removes the temporary.
+			rm -f ${TEMP_SERVICE_CONFIG_FILE}
+			
+		fi
+		
+		# If there is a post deploy script.
+		if [ -f ${CURRENT_MODULE_POST_DEPLOY_SCRIPT} ]
+		then
+		
+			# Runs the post deploy script.
+			${DEBUG} && echo "Running ${CURRENT_MODULE_POST_DEPLOY_SCRIPT}"
+			chmod +x ${CURRENT_MODULE_POST_DEPLOY_SCRIPT} || true
+			${CURRENT_MODULE_POST_DEPLOY_SCRIPT}
+		
+		fi
+		
+		# Goes back to the base dir.
+		cd ..
+		
+	# If the module should not be deployed.	
+	else 
+		# Logs it.
+		echo "Skipping module ${CURRENT_MODULE_NAME}"
 	fi
-	
-	# If there is a post deploy script.
-	if [ -f ${CURRENT_MODULE_POST_DEPLOY_SCRIPT} ]
-	then
-	
-		# Runs the post deploy script.
-		${DEBUG} && echo "Running ${CURRENT_MODULE_POST_DEPLOY_SCRIPT}"
-		chmod +x ${CURRENT_MODULE_POST_DEPLOY_SCRIPT} || true
-		${CURRENT_MODULE_POST_DEPLOY_SCRIPT}
-	
-	fi
-	
-	# Goes back to the base dir.
-	cd ..
 	
 done
 
