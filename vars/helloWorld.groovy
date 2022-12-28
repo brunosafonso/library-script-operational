2b2d773abcb693ca5970979d22cb63ea607bc861
Bruno Afonso <bruno.afonso@supersim.com.br>
2022-12-28 17:38:02 -0300

    testing

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,8 @@
 def call() {
-    sh """
-        echo Hello World updated.
-    """
+    modules = ["service","service-history"]
+    for (int i = 0; i < modules.size(); i++) {
+        sh """
+            echo ${modules[i]}
+        """
+    }
 }
