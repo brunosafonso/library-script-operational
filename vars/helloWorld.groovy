fa08bbed1d9ca03211bed02c543d89d21fb38d98
Bruno Afonso <bruno.afonso@supersim.com.br>
2022-12-30 14:38:39 -0300

    testing

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,8 @@
-def call() {
-    modules = ["service","service-history"]
+def call(Map config = [:]) {
+	multi_service = ["service","service-history"]
     for (int i = 0; i < modules.size(); i++) {
         sh """
+            echo ${config.dir}
             echo ${modules[i]}
         """
     }
